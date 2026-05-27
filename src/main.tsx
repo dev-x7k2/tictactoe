6cd3f12e369c28582839f2cfb07680fd94feeb00
dev-x7k2 <mehdi.el.mestari.pro+easyjob@gmail.com>
2026-05-27 10:17:48 +0200

    fix(config): validate WIN_LENGTH does not exceed GRID_SIZE on startup

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,11 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
+import { WIN_LENGTH, GRID_SIZE } from './config.ts'
+
+if (WIN_LENGTH > GRID_SIZE) {
+    throw new Error(`WIN_LENGTH (${WIN_LENGTH}) cannot be greater than GRID_SIZE (${GRID_SIZE})`);
+}
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
